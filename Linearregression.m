092d9d669602aae00624a734c693a22819d74673
MuziLI2022 <118739032+MuziLI2022@users.noreply.github.com>
2022-12-05 19:54:38 +0800

    Update Linearregression.m

--- Linearregression.m
+++ Linearregression.m
@@ -5,7 +5,11 @@ Es=2000;
 rhobar=[0.05 0.1 0.15 0.2 0.25];
 % Ex=[1. 2. 3. 4. 5.]; %This is going to be our different values of Ex
 Ex=[0.3750 2.9999 10.1245 23.9978 46.8684]; %Ex for hexagon
-% Ex=[33.3333 66.6667 100.0000 133.3333 166.6667]; %Ex for triangle
+% Ex=[33.3356 66.6852 100.0625 133.4814 166.9558]; %Ex for triangle (Option 1)
+% Ex=[33.3333 66.6667 100.0000 133.3333 166.6667]; %Ex for triangle (Option 2)
+% Ey=[0.3750 3.0000 10.1248 23.9993 46.8728]; %Ey for hexagon
+% Ey=[33.3356 66.6852 100.0625 133.4814 166.9558]; %Ey for triangle (Option 1)
+% Ey=[33.3345 66.6759 100.0312 133.4073 166.8111]; %Ey for triangle (Option 2)
 
 for n=1:5
     logrhobar(n)=log(rhobar(n));
